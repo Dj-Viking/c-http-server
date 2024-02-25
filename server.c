284ab795c886936c36edb7e08b3583e9fe5920e4
dj-viking <anders.swedishviking@gmail.com>
2024-02-25 15:18:06 -0500

    let there be a c server :o)

---
+++ server.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void start_server(void)
+{
+    printf("------------------\n");
+    printf("server starting...\n");
+    printf("server has started\nLOL just a test to compile and link everything in windows with cl.exe WOOHOOO\n");
+}
